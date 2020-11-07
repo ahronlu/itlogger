463bababa84ed9a01556311e197ea00a77225020
koftov <ahronlu@gmail.com>
2020-11-07 15:04:56 +0200

    deploy to heroku

---
+++ public/precache-manifest.14e4f42b73588d4c46b214eb0570e2a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbfa1b83651812cf32bf7a718dd31e70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b71855fd0f6754595a06",
+    "url": "/static/css/2.2623327d.chunk.css"
+  },
+  {
+    "revision": "0d99564803e246a35514",
+    "url": "/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "b71855fd0f6754595a06",
+    "url": "/static/js/2.5c5391a6.chunk.js"
+  },
+  {
+    "revision": "c3b72b6745cddcd93b4b2d32212ee19a",
+    "url": "/static/js/2.5c5391a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d99564803e246a35514",
+    "url": "/static/js/main.0209773d.chunk.js"
+  },
+  {
+    "revision": "a3e5487f1ae1f325d5f0",
+    "url": "/static/js/runtime-main.11307dd4.js"
+  }
+]);
